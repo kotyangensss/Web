baf01b23adeedad9d206f1aa820ebe478c67d26c
ktg <optimys.prajm@mail.ru>
2022-12-06 03:02:21 +0300

    feat: added lots of stuff to make cool player

---
+++ amplitude/js/functions.js
@@ -0,0 +1,65 @@
+window.onkeydown = function(e) {
+    return !(e.keyCode == 32);
+};
+
+/*
+  Handles a click on the down button to slide down the playlist.
+*/
+document.getElementsByClassName('down-header')[0].addEventListener('click', function(){
+  var list = document.getElementById('list');
+
+  list.style.height = ( parseInt( document.getElementById('flat-black-player-container').offsetHeight ) - 135 ) + 'px';
+
+  document.getElementById('list-screen').classList.remove('slide-out-top');
+  document.getElementById('list-screen').classList.add('slide-in-top');
+  document.getElementById('list-screen').style.display = "block";
+})
+
+/*
+  Handles a click on the up arrow to hide the list screen.
+*/
+document.getElementsByClassName('hide-playlist')[0].addEventListener('click', function(){
+  document.getElementById('list-screen').classList.remove('slide-in-top');
+  document.getElementById('list-screen').classList.add('slide-out-top');
+  document.getElementById('list-screen').style.display = "none";
+});
+
+/*
+  Handles a click on the song played progress bar.
+*/
+document.getElementById('song-played-progress').addEventListener('click', function( e ){
+  var offset = this.getBoundingClientRect();
+  var x = e.pageX - offset.left;
+
+  Amplitude.setSongPlayedPercentage( ( parseFloat( x ) / parseFloat( this.offsetWidth) ) * 100 );
+});
+
+document.querySelector('img[data-amplitude-song-info="cover_art_url"]').style.height = document.querySelector('img[data-amplitude-song-info="cover_art_url"]').offsetWidth + 'px';
+
+Amplitude.init({
+  "bindings": {
+    37: 'prev',
+    39: 'next',
+    32: 'play_pause'
+  },
+  "songs": [
+    {
+      "name": "I'm so lazy",
+      "artist": "billboards",
+      "url": "songs/billboards - I'm so lazy.mp3",
+      "cover_art_url": "song-art/billboards - I'm so lazy.png"
+    },
+    {
+      "name": "razors",
+      "artist": "billboards",
+      "url": "songs/billboards - razors.mp3",
+      "cover_art_url": "song-art/billboards - razors.png"
+    },
+    {
+      "name": "cold breath",
+      "artist": "billboards",
+      "url": "songs/billboards - cold breath.mp3",
+      "cover_art_url": "song-art/billboards - cold breath.png"
+    }
+  ]
+});
