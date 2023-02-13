87e32bcbe2a2d9cb6863bec5557a001175342605
kotyangensss <optimys.prajm@mail.ru>
2023-02-13 21:08:18 +0300

    feat: added static front

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'process';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
   let port = process.env.PORT;
   if (port === undefined) {
     port = '3000';
