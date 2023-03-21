6511c0a6010810650d8e51919692977c34e6f49a
kotyangensss <optimys.prajm@mail.ru>
2023-03-21 19:54:13 +0300

    feat: lab-3

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 import * as process from 'process';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { PrismaClient } from '@prisma/client';
+import { fieldEncryptionMiddleware } from 'prisma-field-encryption';
 
+export const client = new PrismaClient();
+
+// This is a function, don't forget to call it:
+client.$use(fieldEncryptionMiddleware());
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const hbs = await require('hbs');
